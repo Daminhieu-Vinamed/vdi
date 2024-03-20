5293ccb1d17b2e39b80fff3083935398a4055f9d
unknown <administrator@VMDC.LOC>
2024-03-20 14:57:47 +0700

    editUploadFileJS

--- public/assets/js/upload-files.js
+++ public/assets/js/upload-files.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function ($) {
     jQuery(".upload-files-to-server").click(function () {
-        alert('Do tài liệu tải lên có dung lượng lớn nên hãy chờ trong giây lát')
         var listFile = $("input[name='files']")[0].files;
         formData = new FormData();
         for (var i = 0; i < listFile.length; ++i) {
